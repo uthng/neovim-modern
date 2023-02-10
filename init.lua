6d4aabccd8077cb648899777e98f5fc292e8382f
Thanh NGUYEN <thanh@uthng.me>
2023-02-10 01:36:45 +0100

    Disable autocmd User with VeryLazy event

--- init.lua
+++ init.lua
@@ -1,10 +1,17 @@
 require "config.options"
 require "config.lazy"
+require "config.autocmds"
+require "config.keymaps"
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    require "config.autocmds"
-    require "config.keymaps"
-  end,
-})
+-- Do not use event VeryLazy User because it requires to start mynv
+-- alone to generate VeryLazy event.
+-- In the case of opening directly a file, VeryLazy will be not generated,
+-- and config.autocmds is not loaded.
+--
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "VeryLazy",
+--   callback = function()
+--     require "config.autocmds"
+--     require "config.keymaps"
+--   end,
+-- })
