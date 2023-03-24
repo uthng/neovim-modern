55c549b9fbe9bf16f518fe952423d981a2ac7a2c
Thanh NGUYEN <thanh@uthng.me>
2023-03-24 02:24:24 +0100

    fix: shortcut keymap for disabling terminal mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -120,7 +120,7 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
 
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  -- vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
   vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
   vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
