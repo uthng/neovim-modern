bef72e467e9f0f23559f6bb4269cd54e7bbeb837
Thanh NGUYEN <thanh@uthng.me>
2023-05-13 15:42:04 +0200

    fix(golang): use ray-x/go.nvim for golang

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -97,11 +97,20 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*.rs", "*.go", "*.php", "*.vue", "*.js", "*.ts", "*.tsx" },
+  pattern = { "*.rs", "*.php", "*.vue", "*.js", "*.ts", "*.tsx", "*.py" },
   callback = function()
     vim.lsp.buf.formatting_sync(nil, 200)
   end,
   group = grpStyleIndent,
+})
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    require("go.format").goimport()
+  end,
+  group = format_sync_grp,
 })
 
 vim.api.nvim_create_autocmd({ "CursorHold" }, {
--- lua/plugins/extras/lang/golang.lua
+++ lua/plugins/extras/lang/golang.lua
@@ -1,44 +1,58 @@
 return {
   -- add json to treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "go", "gomod", "gowork" })
-    end,
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, { "go", "gomod", "gowork" })
+  --   end,
+  -- },
 
-  -- correctly setup lspconfig
+  -- -- correctly setup lspconfig
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     -- make sure mason installs the server
+  --     servers = {
+  --       gopls = {
+  --         settings = {
+  --           gopls = {
+  --             hints = {
+  --               assignVariableTypes = true,
+  --               compositeLiteralFields = true,
+  --               compositeLiteralTypes = true,
+  --               constantValues = true,
+  --               functionTypeParameters = true,
+  --               parameterNames = true,
+  --               rangeVariableTypes = true,
+  --             },
+  --             analyses = {
+  --               nilness = true,
+  --               unusedparams = true,
+  --               unusedwrite = true,
+  --               useany = true,
+  --             },
+  --             experimentalPostfixCompletions = true,
+  --             gofumpt = true,
+  --             staticcheck = true,
+  --             usePlaceholders = true,
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- make sure mason installs the server
-      servers = {
-        gopls = {
-          settings = {
-            gopls = {
-              hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-              },
-              analyses = {
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-              },
-              experimentalPostfixCompletions = true,
-              gofumpt = true,
-              staticcheck = true,
-              usePlaceholders = true,
-            },
-          },
-        },
-      },
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
     },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }
