7bcc2844aac5ff0c7a7be248fe512e5a2718ec90
Thanh NGUYEN <thanh@uthng.me>
2023-07-30 11:31:47 +0200

    fix(plugins): use chatgpt custom action file

--- lua/plugins/extras/ai/chatgpt.lua
+++ lua/plugins/extras/ai/chatgpt.lua
@@ -1,11 +1,16 @@
 return {
   "jackMort/ChatGPT.nvim",
   cmd = { "ChatGPT", "ChatGPTRun", "ChatGPTActAs", "ChatGPTCompleteCode", "ChatGPTEditWithInstructions" },
-  config = true,
+  -- config = true,
   enabled = true,
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    actions_paths = {
+      "~/.dotfiles/nvim_chatgpt_actions.json",
+    },
   },
 }
