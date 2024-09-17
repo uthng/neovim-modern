eab8be49cc4517072ae2716e083a1b793996dfa0
Thanh NGUYEN <thanh@uthng.me>
2024-09-18 00:43:41 +0200

    feat(extras): add chatgpt

--- lua/plugins/extras/ai/chatgpt.lua
+++ lua/plugins/extras/ai/chatgpt.lua
@@ -9,6 +9,21 @@ return {
     "nvim-telescope/telescope.nvim",
   },
   opts = {
+    openai_params = {
+      model = "gpt-3.5-turbo",
+      frequency_penalty = 0,
+      presence_penalty = 0,
+      max_tokens = 4096,
+      temperature = 0,
+      top_p = 1,
+      n = 1,
+    },
+    openai_edit_params = {
+      model = "gpt-3.5-turbo",
+      temperature = 0,
+      top_p = 1,
+      n = 1,
+    },
     actions_paths = {
       "~/.dotfiles/nvim_chatgpt_actions.json",
     },
